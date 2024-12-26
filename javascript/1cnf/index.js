835273ac77c07c7a5dc26f7bb34fc8d403cbefcb
Yin Er <yinerkong2082003@gmail.com>
2024-12-26 23:25:24 +0800

    Added FDD-Assignment website files

---
+++ javascript/1cnf/index.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LAPTOP-JT7SL8FN\\user
+vti_modifiedby:SR|LAPTOP-JT7SL8FN\\user
+vti_timelastmodified:TR|24 Nov 2023 18:19:06 -0000
+vti_timecreated:TR|24 Nov 2023 08:08:50 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TR|24 Nov 2023 18:18:55 -0000
+vti_cacheddtm:TX|24 Nov 2023 18:19:06 -0000
+vti_filesize:IR|1904
---
+++ javascript/index.js
@@ -0,0 +1,49 @@
+﻿window.onload = document.onload = () => {
+    var questions = document.getElementsByClassName('question');
+    for (let i = 0; i < questions.length; i++) {
+        const e = questions[i];
+        e.children[0].addEventListener('click', (f) => {
+            onClickSummaryButton(f, e);
+        });
+    }
+}
+function onClickSummaryButton(event, questionElement){
+    event.preventDefault();
+    questionElement.style.overflow = 'hidden';
+
+    if(!questionElement.open){
+        questionElement.style.height = `${questionElement.offsetHeight}px`
+        questionElement.open = true;
+
+        var contentHeight = questionElement.children[1];
+        contentHeight = contentHeight.offsetHeight + parseInt(window.getComputedStyle(contentHeight).getPropertyValue('margin-top')) + parseInt(window.getComputedStyle(contentHeight).getPropertyValue('margin-bottom'));
+
+        window.requestAnimationFrame(() => {
+            const startHeight = `${questionElement.offsetHeight}px`;
+            const endHeight = `${questionElement.children[0].offsetHeight + contentHeight}px`;
+
+            var animation = questionElement.animate({
+                height: [startHeight, endHeight]
+            }, {
+                duration: 200,
+                easing: 'linear'
+            })
+            animation.onfinish = () => {
+                questionElement.style.height = questionElement.style.overflow = '';
+            }
+        })
+
+    } else {
+        var animation = questionElement.animate({
+            height: [`${questionElement.offsetHeight}px`, `${questionElement.children[0].offsetHeight}px`]
+        }, {
+            duration: 200,
+            easing: 'linear'
+        });
+        
+        animation.onfinish = () => {
+            questionElement.style.height = questionElement.style.overflow = '';
+            questionElement.open = false;
+        }
+    }
+}
